3e9913dd6b7f0b1aa45309e3b2cbadcb8e86882b
David <davestacey@users.noreply.github.com>
2021-09-18 20:37:09 -0400

    feat: some buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./styles.css";
+
+const MyButton = (props) => {
+  const [count, setCount] = useState(0);
+  return (
+    <div
+      style={{
+        color: props.color,
+        textDecoration: props.underline ? 'underline' : undefined
+      }}
+      onClick={() => setCount(c => c + props.increment)}>
+      I am button {count}
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <MyButton increment={2} underline={true} color='blue' />
+      <MyButton increment={2} underline={true} color='red' />
+      <MyButton increment={2} underline={true} color='orange' />
     </div>
   );
 }
