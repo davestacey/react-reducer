884fcb36b3d1e3deb7082290c86d845c4797d750
David <davestacey@users.noreply.github.com>
2021-09-18 21:26:58 -0400

    feat: sharedProps, useReducer fun

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import "./styles.css";
 
+const defaultProps = {
+  increment: 2,
+  underline: true,
+  color: 'black'
+}
+
 const MyButton = (props) => {
-  const [count, setCount] = useState(0);
+  const [countA, setCountA] = useState(0);
+  const [countB, setCountB] = useReducer((total, incrementBy) => {
+    return total + incrementBy;
+  }, 0);
+
   return (
     <div
       style={{
         color: props.color,
         textDecoration: props.underline ? 'underline' : undefined
       }}
-      onClick={() => setCount(c => c + props.increment)}>
-      I am button {count}
+      onClick={() => {
+        setCountA(c => c + props.increment);
+        setCountB(1);
+      }}>
+      useReducer={countB} useState={countA}
     </div>
   );
 }
@@ -18,9 +31,9 @@ const MyButton = (props) => {
 export default function App() {
   return (
     <div className="App">
-      <MyButton increment={2} underline={true} color='blue' />
-      <MyButton increment={2} underline={true} color='red' />
-      <MyButton increment={2} underline={true} color='orange' />
+      <MyButton {...defaultProps} color='blue' />
+      <MyButton {...defaultProps} underline={false} color='red' />
+      <MyButton {...defaultProps} color='orange' />
     </div>
   );
 }
