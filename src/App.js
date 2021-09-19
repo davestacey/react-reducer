fbc89b56103d3d900f8d05081183ee207221d9c7
David <davestacey@users.noreply.github.com>
2021-09-18 22:47:08 -0400

    feat: adds useReducer and useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState, useReducer } from "react";
+import React, { useState, useReducer, useRef } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import "./styles.css";
 
 const defaultProps = {
@@ -28,12 +29,64 @@ const MyButton = (props) => {
   );
 }
 
+const MyForm = () => {
+  const inputRef = useRef();
+  const [items, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "add":
+        return [
+          ...state,
+          {
+            id: uuidv4(),
+            name: action.name
+          }
+        ];
+      case "remove":
+        // keep every item except the one we want to remove
+        return state.filter((_, index) => index != action.index);
+      case "clear":
+        return [];
+      default:
+        return state;
+    }
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({
+      type: "add",
+      name: inputRef.current.value
+    });
+    inputRef.current.value = "";
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input ref={inputRef} />
+      </form>
+      <button onClick={() => dispatch({ type: "clear" })}>Clear</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            {item.name}{" "}
+            <button onClick={() => dispatch({ type: "remove", index })}>
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+    );
+}
+
 export default function App() {
   return (
     <div className="App">
       <MyButton {...defaultProps} color='blue' />
       <MyButton {...defaultProps} underline={false} color='red' />
       <MyButton {...defaultProps} color='orange' />
+      <MyForm></MyForm>
     </div>
   );
 }
